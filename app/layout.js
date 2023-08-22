ef6b325b393be3d649273812a083913e1215a2db
lalovar <edvarluc@gmail.com>
2023-08-21 23:20:43 -0600

    templates

--- app/layout.js
+++ app/layout.js
@@ -1,25 +1,25 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import logo from '../public/logo.jpg'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import logo from "../public/logo.jpg";
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Boda Michelle y Salvador',
-  description: 'Página de confirmación de asistencia a la boda de Michelle y Salvador',
-  image: logo
-}
+  title: "Boda Michelle y Salvador",
+  description:
+    "Página de confirmación de asistencia a la boda de Michelle y Salvador",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta property="og:image" content={logo} />
       <link rel="icon" href="/icon.png" sizes="any" />
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,10 +1,47 @@
-import * as React from 'react';
-import ConfirmForm from '../components/ConfirmForm';
+import * as React from "react";
+import ConfirmForm from "../components/ConfirmForm";
+import bg1 from "../public/img2.jpg";
 
 export default function Page() {
   return (
     <main className={"some"}>
-        <ConfirmForm />
+      <div style={{ backgroundImage: `url(${bg1.src})` }} className="header">
+        <h1>
+          Michelle y Salvador
+          <span>16.12.2023</span>
+        </h1>
+      </div>
+      <div className="phrase">
+        <p>Este es el sueño que compartimos...</p>
+        <p>El que queremos cuidar a través de los años...</p>
+        <p>El que hablará por nosotros, mas que toda palabra...</p>
+        <p>El que nos reencontrará a través de cualquier distancia...</p>
+        <p>Este es el sueño que compartimos, y se llama amor.</p>
+      </div>
+      <div className="phrase2">
+        <p>
+          Con la presencia de dios entre nosotros y la bendicion de nuestros
+          padres
+        </p>
+        <table>
+          <tr>
+            <td>Miguel Angel Cuevas Maciel</td>
+            <td>† Salvador Ceja Ramos</td>
+          </tr>
+          <tr>
+            <td>Ana Laura Hernandez Castellanos</td>
+            <td>Ma. Guadalupe Garcia Fuentes</td>
+          </tr>
+        </table>
+        <span>
+          <p>
+            Te invitamos a formar parte de nuestra boda y acompañarnos a
+            celebrar el inicio de nuestra vida juntos
+          </p>
+          <h3>16/12/2023</h3>
+        </span>
+      </div>
+      <ConfirmForm />
     </main>
   );
 }
