23ca8287fe241032dfc36156e3249759a34e6578
lalovar <edvarluc@gmail.com>
2023-08-22 21:32:24 -0600

    fixes

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import ConfirmForm from "../components/ConfirmForm";
 import bg1 from "../public/img2.jpg";
 import bg2 from "../public/img1.jpg";
 import WeedingTimeline from "../components/WeedingTimeline";
+import { Typography } from "@mui/material";
 
 export default function Page() {
   return (
@@ -13,7 +14,7 @@ export default function Page() {
           <span>16.12.2023</span>
         </h1>
       </div>
-      <div className="phrase" style={{ backgroundImage: `url(${bg2.src})` }} >
+      <div className="phrase">
         <p>Este es el sueño que compartimos...</p>
         <p>El que queremos cuidar a través de los años...</p>
         <p>El que hablará por nosotros, mas que toda palabra...</p>
@@ -26,14 +27,16 @@ export default function Page() {
           padres
         </p>
         <table>
-          <tr>
-            <td>Miguel Angel Cuevas Maciel</td>
-            <td>† Salvador Ceja Ramos</td>
-          </tr>
-          <tr>
-            <td>Ana Laura Hernandez Castellanos</td>
-            <td>Ma. Guadalupe Garcia Fuentes</td>
-          </tr>
+          <tbody>
+            <tr>
+              <td>Miguel Angel Cuevas Maciel</td>
+              <td>† Salvador Ceja Ramos</td>
+            </tr>
+            <tr>
+              <td>Ana Laura Hernandez Castellanos</td>
+              <td>Ma. Guadalupe Garcia Fuentes</td>
+            </tr>
+          </tbody>
         </table>
         <span>
           <p>
@@ -45,6 +48,11 @@ export default function Page() {
         <WeedingTimeline />
       </div>
       <div className="confirmForm">
+        <Typography variant="h4">Confirma tu asistencia</Typography>
+        <Typography variant="body2">
+          Te invitamos cordialmete a ser parte de nuestro amor en este día tan
+          especial.
+        </Typography>
         <ConfirmForm />
       </div>
     </main>
